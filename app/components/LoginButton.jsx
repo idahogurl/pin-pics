9b39716ae600248f77c03f491a1a225efb2e17b1
Rebecca Vest <rebecca.vest@stukent.com>
2019-11-16 23:25:59 -0700

    Update FB

--- app/components/LoginButton.jsx
+++ app/components/LoginButton.jsx
@@ -6,17 +6,19 @@ import processResponse from '../utils/facebookResponse';
 
 const onLogin = async function onLogin(response) {
   const { id } = response;
-  const currentUser = id.toString();
-  sessionStorage.setItem('currentUser', currentUser);
+  if (id) {
+    const currentUser = id.toString();
+    sessionStorage.setItem('currentUser', currentUser);
 
-  await processResponse(response);
-  window.location = `/${currentUser}`;
+    await processResponse(response);
+    window.location = `/${currentUser}`;
+  }
 };
 
 export default function LoginButton() {
   return (
     <FacebookLogin
-      appId="323585701513857"
+      appId="2524623997651700"
       callback={onLogin}
       fields="name,email"
       render={({ isWorking, isLoading, onClick }) => (
